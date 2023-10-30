8e4543e4b4d74f7b3a11250bb1b5b217d0f24fd4
Jobenas <jobenas@energyatech.com>
2023-10-30 17:56:39 +0000

    semana 12 parte 3

--- generador_notas_sync.py
+++ generador_notas_sync.py
@@ -15,8 +15,31 @@ def genera_labs():
             f.write(fila)
 
 
+def genera_parcial():
+     with open("notas_parcial.csv", "w+", encoding='utf-8') as f:
+        f.write("codigo,nota\n")
+        codigo_inicial = 20230001
+        for i in range(100):
+            fila = f"{codigo_inicial + i},{random.randint(0, 20)}\n"
+            f.write(fila)
+
+
+def genera_final():
+     with open("notas_final.csv", "w+", encoding='utf-8') as f:
+        f.write("codigo,nota\n")
+        codigo_inicial = 20230001
+        for i in range(100):
+            fila = f"{codigo_inicial + i},{random.randint(0, 20)}\n"
+            f.write(fila)
+
+
 def main():
+    print("Generando notas de lab")
     genera_labs()
+    print("Generando notas de parcial")
+    genera_parcial()
+    print("Generando notas de final")
+    genera_final()
 
 
 if __name__ == '__main__':
